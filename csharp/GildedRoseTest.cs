b7b08583140b2d658fcbb8035ebd595dac3a2609
xeti6 <honza1996@gmail.com>
2018-05-13 12:55:41 +0200

    TestRegularItem added

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -6,13 +6,20 @@ namespace csharp
     [TestFixture]
     public class GildedRoseTest
     {
+
+
         [Test]
-        public void foo()
+        public void testRegularItem()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            var items = new List<Item> {new Item {Name = "meat", SellIn = 10, Quality = 25}};
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual(items[0].Name, "meat");
+            Assert.AreEqual(items[0].Quality, 24);
+            Assert.AreEqual(items[0].SellIn, 9);
+
         }
+
+
     }
 }
